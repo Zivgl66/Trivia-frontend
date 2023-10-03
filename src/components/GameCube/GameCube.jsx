69179bb208b8298c038c7b45c706ca7751a28ab8
Zivgl66 <95753868+Zivgl66@users.noreply.github.com>
2023-10-03 22:28:06 +0300

    when clicking on game start - get game id

--- src/components/GameCube/GameCube.jsx
+++ src/components/GameCube/GameCube.jsx
@@ -12,7 +12,7 @@ const GameCube = ({ game, startGame }) => {
         <button
           type="button"
           className="btn btn-outline-success"
-          onClick={() => startGame(game.id)}
+          onClick={() => startGame(game._id)}
         >
           Start
         </button>
